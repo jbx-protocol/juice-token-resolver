eb71518ae3dcff3814a087601a58d68b3d93ceaf
nicholas <nnnnicholas@users.noreply.github.com>
2023-02-27 12:00:22 -0500

    Added license

--- src/TokenUriResolver.sol
+++ src/TokenUriResolver.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import {IJBTokenUriResolver} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBTokenUriResolver.sol";
