0235bef879e606537df6d9874f8d005143912eed
nicholas <nnnnicholas@users.noreply.github.com>
2023-03-01 15:47:22 -0500

    remove superfluous theme loading

--- src/DefaultTokenUriResolver.sol
+++ src/DefaultTokenUriResolver.sol
@@ -402,40 +402,14 @@ contract DefaultTokenUriResolver is IJBTokenUriResolver, JBOperatable {
             ); // Abbreviate owner address
     }
 
-    function getTheme(uint256 _projectId)
-        internal
-        view
-        returns (
-            bytes3 textColor,
-            bytes3 bgColor,
-            bytes3 bgColorDark
-        )
-    {
-        if (themes[_projectId].projectId == 0) {
-            return (
-                themes[0].textColor,
-                themes[0].bgColor,
-                themes[0].bgColorDark
-            );
-        } else {
-            return (
-                themes[_projectId].textColor,
-                themes[_projectId].bgColor,
-                themes[_projectId].bgColorDark
-            );
-        }
-    }
-
     function getUri(uint256 _projectId)
         external
         view
         override
         returns (string memory tokenUri)
     {
-        // Load theme
-        (bytes3 textColor, bytes3 bgColor, bytes3 bgColorDark) = getTheme(
-            _projectId
-        );
+
+        Theme memory theme = themes[_projectId].projectId == 0 ? themes[0] : themes[_projectId];
 
         // Funding Cycle
         // FC#
@@ -501,11 +475,11 @@ contract DefaultTokenUriResolver is IJBTokenUriResolver, JBOperatable {
                 '<svg width="289" height="403" viewBox="0 0 289 403" xmlns="http://www.w3.org/2000/svg"><style>@font-face{font-family:"Capsules-500";src:url(data:font/truetype;charset=utf-8;base64,',
                 getFontSource(), // import Capsules typeface
                 ');format("opentype");}a,a:visited,a:hover{fill:inherit;text-decoration:none;}text{font-size:16px;fill:#',
-                textColor,
+                theme.textColor,
                 ';font-family:"Capsules-500",monospace;font-weight:500;white-space:pre;}#head text{fill:#',
-                bgColor,
+                theme.bgColor,
                 ';}</style><g clip-path="url(#clip0)"><path d="M289 0H0V403H289V0Z" fill="url(#paint0)"/><rect width="289" height="22" fill="#',
-                textColor,
+                theme.textColor,
                 '"/><g id="head"><a href="https://juicebox.money/v2/p/',
                 _projectId.toString(),
                 '">', // Line 0: Head
@@ -548,13 +522,13 @@ contract DefaultTokenUriResolver is IJBTokenUriResolver, JBOperatable {
                 ownerName,
                 "</a>",
                 '</text></g></g><defs><filter id="filter1" x="-3.36" y="26.04" width="294.539" height="126.12" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/><feOffset/><feGaussianBlur stdDeviation="2"/><feComposite in2="hardAlpha" operator="out"/> <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 0.572549 0 0 0 0 0.0745098 0 0 0 0.68 0"/><feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_150_56"/><feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_150_56" result="shape"/></filter><linearGradient id="paint0" x1="0" y1="202" x2="289" y2="202" gradientUnits="userSpaceOnUse"><stop stop-color="#',
-                bgColorDark,
+                theme.bgColorDark,
                 '"/><stop offset="0.119792" stop-color="#',
-                bgColor,
+                theme.bgColor,
                 '"/><stop offset="0.848958" stop-color="#',
-                bgColor,
+                theme.bgColor,
                 '"/><stop offset="1" stop-color="#',
-                bgColorDark,
+                theme.bgColorDark,
                 '"/></linearGradient><clipPath id="clip0"><rect width="289" height="403" /></clipPath></defs></svg>'
             )
         );
