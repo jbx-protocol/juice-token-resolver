7df6540c017f9db5898b51ca6b43097be428a8dd
nicholas <nnnnicholas@users.noreply.github.com>
2023-03-15 18:38:59 -0400

    Comments: Theme

--- src/Structs/Theme.sol
+++ src/Structs/Theme.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.0;
 import {Color} from "solcolor/src/Types.sol";
 
 /**
- * @member customTheme True for all Themes except the default theme
+ * @notice Themes contain color data for styling SVGs.
+ * @member customTheme True for all Themes except the default theme.
  * @member textColor The color of the text.
  * @member bgColor The primary background color.
  * @member bgColorAlt The secondary background color.
